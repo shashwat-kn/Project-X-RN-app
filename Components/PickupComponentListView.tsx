ba24a538ad336f62b11b63bacde90bffea53c8b8
shashwat kn <shashwat.kn@swiggy.in>
2021-05-24 11:41:31 +0530

    adding android toast

---
+++ Components/PickupComponentListView.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { FlatList, RefreshControl } from "react-native";
+import newResponse from "../Mocks/hResponse";
+import PickupComponent from "./PickUpComponent";
+
+interface PickupComponentListViewProps {
+    refreshing: boolean
+    uploadPhoto: (id: string) => void
+    onRefresh: () => void
+}
+export default class PickupComponentListView extends React.Component<PickupComponentListViewProps> {
+    render() {
+        return (
+            <FlatList
+                refreshControl={
+                    <RefreshControl
+                        refreshing={this.props.refreshing}
+                        onRefresh={this.props.onRefresh}
+                    />
+                }
+                data={newResponse.response.data}
+                renderItem={({ item }) => <PickupComponent {...item} uploadPhoto={this.props.uploadPhoto} />}
+            />
+        );
+    }
+}
